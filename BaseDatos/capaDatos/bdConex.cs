0a6141952d1feb0dd793655e9d6a964eca9da438
RJVasquez19 <47983895+RJVasquez19@users.noreply.github.com>
2019-05-14 15:20:29 -0600

    Add files via upload

---
+++ BaseDatos/capaDatos/bdConex.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data.Sql;
+
+namespace capaDatos
+{
+    public class bdConex
+    {
+        SqlDataAdapter adaptador;
+        SqlCommandBuilder comm;
+        SqlConnection conexion = new SqlConnection();
+        SqlCommand comando;
+
+        String cadenaConex = "Data Source=localhost;Initial Catalog=DBFACTURAS;User ID=julio_perez2;Password=123";
+
+        public SqlConnection Conexion
+        {
+            get { return conexion; }
+            set { conexion = value; }
+        }
+
+        public bool Conectar()
+        {
+            bool conn = false;
+            try
+            {
+                conexion.ConnectionString = cadenaConex;
+                conexion.Open();
+                conn = true;
+            }catch(Exception ex)
+            {
+                conn = false;
+            }
+
+            return conn;
+        }
+
+        public void Desconectar() 
+        {
+            conexion.Close();
+        }
+
+        public void consultaSQL(String consulta) 
+        {
+            String mensaje = "";
+            try 
+            {
+                comando = new SqlCommand(consulta, conexion);
+                comando.ExecuteNonQuery();
+                mensaje = "Correctamente";
+            }
+            catch(Exception e)
+            {
+                mensaje = "Error";
+            }
+        }
+    }
+}
---
+++ BaseDatos/capaNegocios/manttoVendor.cs
@@ -0,0 +1,90 @@
+﻿using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaNegocios
+{
+    public class manttoVendor
+    {
+        bdConex test = new bdConex();
+
+        public String prueba() 
+        {
+            String mensaje;
+
+            if(test.Conectar() == true)
+            {
+                test.Desconectar();
+                mensaje = "Conexion realizada";
+            }else
+            {
+                test.Desconectar();
+                mensaje = "Conexion no realizada";
+            }
+            return mensaje;
+        }
+
+        String codigoVendor, nombreVendor;
+
+        public String NombreVendor
+        {
+            get { return nombreVendor; }
+            set { nombreVendor = value; }
+        }
+
+        public String CodigoVendor
+        {
+            get { return codigoVendor; }
+            set { codigoVendor = value; }
+        }
+
+        public void insertarVendor() 
+        {
+            try 
+            {
+                String insert = "insert into VENDEDOR values('" + codigoVendor +"','" + nombreVendor + "');";
+                test.Conectar();
+                test.consultaSQL(insert);
+                test.Desconectar();
+            }
+            catch 
+            {
+            }
+        }
+
+        public void updateVendor() 
+        {
+            try 
+            {
+                String update = "update VENDEDOR set NOMBRE='" + nombreVendor + "' where VENDEDOR='" + codigoVendor + "'";
+
+                test.Conectar();
+                test.consultaSQL(update);
+                test.Desconectar();
+            }
+            catch 
+            {
+            }
+        }
+
+        //////////////
+
+        public void consultar() 
+        {
+            try { 
+            String select = "select '" + codigoVendor + "' '" + nombreVendor + "' from VENDEDOR where VENDEDOR = '" + codigoVendor +"'";
+            test.Conectar();
+            test.consultaSQL(select);
+            test.Desconectar();
+            }
+            catch (Exception e) 
+            {
+            }
+        }
+
+        ////////////////
+    }
+}
